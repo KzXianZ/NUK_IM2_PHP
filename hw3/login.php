5a533dfe8ad4ae327a35dc8788f844c039404135
KzXian <steven.yong0423@gmail.com>
2025-03-24 19:03:13 +0800

    [hw3] modify layout

--- hw3/login.php
+++ hw3/login.php
@@ -2,10 +2,10 @@
 
 <h1>welcome! Please login</h1>
 
-<form action="logincheck.php" method="POST">
+<form action = "logincheck.php" method = "POST">
 
-please enter the name:<input type="text" name="name"><BR/>
-please enter the password:<input type="password" name="pwd"><BR/>
-<input type="submit"><BR/>
+please enter the name:<input type = "text" name = "name"><BR/>
+please enter the password:<input type = "password" name = "pwd"><BR/>
+<input type = "submit"><BR/>
 
 </form>
--- hw3/logincheck.php
+++ hw3/logincheck.php
@@ -6,28 +6,30 @@ session_start();
 
 <?php
 
-$uName="1123340";
-$uPwd="12345";
+$uName = "1123340";
+$uPwd = "12345";
 
-$aName="nuk";
-$aPwd="1234";
+$aName = "nuk";
+$aPwd = "1234";
 
-$userName=$_POST["name"];
-$userPwd=$_POST["pwd"];
+$userName = $_POST["name"];
+$userPwd = $_POST["pwd"];
 
-if($uName==$userName && $uPwd==$userPwd){
+if ($uName == $userName && $uPwd == $userPwd) {
     echo "student login success";
-    $_SESSION["user"]=1;
-    $date=strtotime("+ 10 seconds",time());
-    setcookie("name",$userName,$date);
+    $_SESSION["user"] = 1;
+    $date = strtotime("+ 10 seconds",time());
+    setcookie("name", $userName,$date);
     header("location:userform.php");
-}if($aName==$userName && $aPwd==$userPwd){
+}
+if ($aName == $userName && $aPwd == $userPwd) {
     echo "admin login success";
-    $_SESSION["admin"]=1;
-    $date=strtotime("+ 10 seconds",time());
-    setcookie("name",$userName,$date);
+    $_SESSION["admin"] = 1;
+    $date = strtotime("+ 10 seconds",time());
+    setcookie("name", $userName,$date);
     header("location:adminform.php");
-}else{
+}  
+else {
     echo "login failed";
     header("refresh:3;url=login.php");
 }
